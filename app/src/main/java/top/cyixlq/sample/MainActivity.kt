f6ffb60b0e50c02ec830050ef625facba52fc83f
cyixlq <2421597157@qq.com>
2021-04-16 15:51:47 +0800

    增加自定义活跃状态需求

--- app/src/main/java/top/cyixlq/sample/MainActivity.kt
+++ app/src/main/java/top/cyixlq/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import top.cyixlq.liveeventbus.LiveEventBus
 import java.lang.Thread.sleep
@@ -13,31 +14,43 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
 
     private var times = 0
-    private lateinit var observer: Observer<Int>
+    private var observer: Observer<Int>
+
+    init {
+        LiveEventBus.get().with(Int::class.java).postEvent(-1)
+        LiveEventBus.get().with(Int::class.java).observe(this) {
+            Log.d(TAG, "MainActivity onCreate observe接收到的值：$it，state：${lifecycle.currentState}")
+        }
+        LiveEventBus.get().with(Int::class.java).observeSticky(this) {
+            Log.d(TAG, "MainActivity onCreate observeSticky接收到的值：$it，state：${lifecycle.currentState}")
+        }
+        observer = Observer<Int> {
+            Log.d(TAG, "MainActivity onCreate observeForever接收到的值：$it，state：${lifecycle.currentState}")
+        }
+        LiveEventBus.get().with(Int::class.java).observeForever(observer)
+        LiveEventBus.get().with("EVENT_KEY", String::class.java).observe(this) {
+            Log.d(TAG, "MainActivity onCreate 通过String作为key订阅接收到的值：$it")
+        }
+        LiveEventBus.get().with(Int::class.java).observeCustom(this, Lifecycle.State.RESUMED) {
+            Log.d(TAG, "MainActivity onResume observe接收到的值：$it，state：${lifecycle.currentState}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        LiveEventBus.get().with(Int::class.java).observe(this) {
-            Log.d("TAG", "MainActivity observe接收到的值：$it")
-        }
-        observer = Observer<Int> {
-            Log.d("TAG", "MainActivity observeForever接收到的值：$it")
-        }
-        LiveEventBus.get().with(Int::class.java).observeForever(observer)
-        LiveEventBus.get().with("EVENT_KEY", String::class.java).observe(this) {
-            Log.d("TAG", "MainActivity 通过String作为key订阅接收到的值：$it")
-        }
         thread {
             while (Thread.currentThread().isAlive) {
-                sleep(5000)
+                // 直接通过数据类型来发送事件
                 LiveEventBus.get().with(Int::class.java).postEvent(times++)
+                sleep(5000)
             }
         }
     }
 
     fun onTvClick(v: View) {
         LiveEventBus.get().with(Int::class.java).postEvent(times++)
+        // 通过key发送事件
         LiveEventBus.get().with("EVENT_KEY", String::class.java).postEvent("event")
     }
 
@@ -48,5 +61,9 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         LiveEventBus.get().with(Int::class.java).removeObserver(observer)
+    }
+
+    companion object {
+        const val TAG = "TAG"
     }
 }
